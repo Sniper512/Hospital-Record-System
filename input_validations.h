2e68c58a5ad3119a97743d6f6447597a46cb91d9
Syed M.Jafar Ali Naqvi <103287122+Sniper512@users.noreply.github.com>
2022-12-06 02:22:57 +0500

    input validation modifications
    validation modifications done

--- input_validations.h
+++ input_validations.h
@@ -15,14 +15,15 @@ struct valid_struct
 };
 void write_total_records()
 {
-    FILE *fp=NULL;
-    fp=fopen("Total_records.txt", "w");
-    if(fp==NULL)
+    FILE *fp = NULL;
+    fp = fopen("Total_records.txt", "w");
+    if (fp == NULL)
     {
         printf("Error in reading file.\n");
     }
-    else{
-        fprintf(fp,"%u",total_record);
+    else
+    {
+        fprintf(fp, "%u", total_record);
     }
     fclose(fp);
 }
@@ -31,28 +32,29 @@ void spaces(char array[])
     int length = strlen(array);
     for (int i = length + 1; i < 30; i++)
     {
-        array[i] =32;
+        array[i] = 32;
     }
 }
 
 int phoneNumber_checker(char array[])
 {
     int check = 1;
-    if (strlen(array) > 13 || strlen(array) <12)
+    if (strlen(array) > 13 || strlen(array) < 12)
     {
         return 0;
     }
     for (int i = 0; i < strlen(array) - 1; i++)
     {
-        if (i == 0 || i == 1 || i==4)
+        if (i == 0 || i == 1 || i == 4)
         {
-            if (array[i] != '0' || array[i] != '3' || array[i] != '-');
+            if (array[i] != '0' || array[i] != '3' || array[i] != '-')
+                ;
             {
                 check = 0;
             }
         }
         char num = array[i];
-        if ((num >= 48 && num <= 57)|| num==45)
+        if ((num >= 48 && num <= 57) || num == 45)
         {
             check = 1;
         }
@@ -66,16 +68,17 @@ int phoneNumber_checker(char array[])
 }
 int disease_checker(char array[])
 {
-    int check=1;
-    for(int i=0;i<strlen(array)-1;i++)
+    int check = 1;
+    for (int i = 0; i < strlen(array) - 1; i++)
     {
-        char num= array[i];
-        if((num>= 65 && num<=90)||(num>=97 && num<=122) || num==32)
+        char num = array[i];
+        if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122) || num == 32)
         {
-           check=1;
+            check = 1;
         }
-        else{
-            check=0;
+        else
+        {
+            check = 0;
             break;
         }
     }
@@ -84,7 +87,7 @@ int disease_checker(char array[])
 int cnic_checker(char array[])
 {
     int check = 1;
-    if (strlen(array) > 17)
+    if (strlen(array) > 15 || strlen(array) < 15)
     {
         return 0;
     }
@@ -98,6 +101,11 @@ int cnic_checker(char array[])
             {                    // 012345678912345
                 return 0;
             }
+        }
+        else if (i != 5 && i != 13)
+        {
+            if (array[i] == '-')
+                return 0;
         }
         char num = array[i];
         if ((num >= 48 && num <= 57) || num == 45)
@@ -118,7 +126,7 @@ int name_checker(char array[])
     for (size_t i = 0; i < strlen(array); i++)
     {
         char num = array[i];
-        if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122) ||num==32)
+        if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122) || num == 32)
         {
             check = 1;
         }
@@ -150,30 +158,30 @@ int repeat_check(char id[])
 }
 int admit_check(char array[])
 {
-    char str1[]="Yes";
-    char str2[]="yes";
-    char str3[]="no";
-    char str4[]="No";
-    if(strcmp(array,str1)==0)
+    char str1[] = "Yes";
+    char str2[] = "yes";
+    char str3[] = "no";
+    char str4[] = "No";
+    if (strcmp(array, str1) == 0)
     {
         return 1;
     }
-    else if(strcmp(array,str2)==0)
+    else if (strcmp(array, str2) == 0)
     {
         return 1;
     }
-    else if(strcmp(array,str3)==0)
+    else if (strcmp(array, str3) == 0)
     {
         return 1;
     }
-    else if(strcmp(array,str4)==0)
+    else if (strcmp(array, str4) == 0)
     {
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
-    
 }
 int id_checker(char array[])
 {
